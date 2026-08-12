c2738201a34e4a1156105e8dec618bd0abffcd80
Amirul <amirulazw94@gmail.com>
2026-08-13 00:45:25 +0800

    mobile header fix

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    ============================================================ */
 'use strict';
 
-var CACHE = 'paie-cache-v3';
+var CACHE = 'paie-cache-v4';
 var CORE = [
   './',
   './index.html',
